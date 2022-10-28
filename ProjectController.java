b08896a06f7a3907913feff115285dfb170b324a
LynnYue10 <116870883+LynnYue10@users.noreply.github.com>
2022-10-28 15:47:15 +0800

    add1

---
+++ ProjectController.java
@@ -0,0 +1,109 @@
+package com.aim.questionnaire.controller;
+
+import com.aim.questionnaire.beans.HttpResponseEntity;
+import com.aim.questionnaire.common.Constans;
+import com.aim.questionnaire.common.utils.UUIDUtil;
+import com.aim.questionnaire.dao.entity.ProjectEntity;
+import com.aim.questionnaire.service.ProjectService;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by wln on 2018\8\6 0006.
+ */
+@RestController
+public class ProjectController {
+
+    private final Logger logger = LoggerFactory.getLogger(ProjectController.class);
+
+    @Autowired
+    private ProjectService projectService;
+
+
+    /**
+     * 查询全部项目的信息
+     * @param projectEntity
+     * @return
+     */
+    @RequestMapping(value = "/queryProjectList",method = RequestMethod.POST, headers = "Accept=application/json")
+    public HttpResponseEntity queryProjectList(@RequestBody(required = false) ProjectEntity projectEntity) {
+        HttpResponseEntity httpResponseEntity = new HttpResponseEntity();
+        List<Object> list = projectService.queryProjectList(projectEntity);
+        httpResponseEntity.setCode(Constans.SUCCESS_CODE);
+        httpResponseEntity.setData(list);
+        httpResponseEntity.setMessage(Constans.STATUS_MESSAGE);
+        return httpResponseEntity;
+    }
+
+    /**
+     * 根据id删除项目
+     * @param projectEntity
+     * @return
+     */
+    @RequestMapping(value = "/deleteProjectById",method = RequestMethod.POST, headers = "Accept=application/json")
+    public HttpResponseEntity deleteProjectById(ProjectEntity projectEntity) {
+        HttpResponseEntity httpResponseEntity = new HttpResponseEntity();
+           
+        return httpResponseEntity;
+    }
+
+    /**
+     * 添加项目
+     * @param projectEntity
+     * @return
+     */
+    @RequestMapping(value = "/addProjectInfo",method = RequestMethod.POST, headers = "Accept=application/json")
+    public HttpResponseEntity addProjectInfo(@RequestBody ProjectEntity projectEntity) {
+        HttpResponseEntity httpResponseEntity = new HttpResponseEntity();
+        /*List<Map<String,Object>> maps = projectService.queryProjectList(projectEntity);
+        if(!CollectionUtils.isEmpty(maps)) {
+        	httpResponseEntity.setCode("111");
+        }else {
+        	/*
+        	 * 新增项目到mysql
+        	 * 
+        	projectEntity.setId(UUIDUtil.getOneUUID());
+        	projectEntity.setCreationDate(new Date());
+        	projectService.addProjectInfo(projectEntity, null);
+        	httpResponseEntity.setCode("666");
+        }*/
+            
+        return httpResponseEntity;
+    }
+
+    /**
+     * 根据项目id修改项目
+     * @param projectEntity
+     * @return
+     */
+    @RequestMapping(value = "/modifyProjectInfo",method = RequestMethod.POST, headers = "Accept=application/json")
+    public HttpResponseEntity modifyProjectInfo(@RequestBody ProjectEntity projectEntity) {
+        HttpResponseEntity httpResponseEntity = new HttpResponseEntity();
+            
+        return httpResponseEntity;
+    }
+
+
+
+    /**
+     * 查询全部项目的名字接口
+     * @return
+     */
+    @RequestMapping(value = "/queryAllProjectName",method = RequestMethod.POST, headers = "Accept=application/json")
+    public HttpResponseEntity queryAllProjectName() {
+        HttpResponseEntity httpResponseEntity = new HttpResponseEntity();
+          
+        return httpResponseEntity;
+    }
+}
